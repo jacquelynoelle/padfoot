602479beac94e7d8666d1456331674f2900a729c
jackie <jacquelyn.cheng@me.com>
2019-01-12 13:19:50 -0800

    BLE scan now working with recyclerview

--- app/src/main/java/io/github/jacquelynoelle/padfoot/BLEDeviceAdapter.java
+++ app/src/main/java/io/github/jacquelynoelle/padfoot/BLEDeviceAdapter.java
@@ -12,40 +12,12 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
 public class BLEDeviceAdapter extends RecyclerView.Adapter<BLEDeviceAdapter.DeviceViewHolder> {
 
     private static final String TAG = BLEDeviceAdapter.class.getSimpleName();
-
-    // COMPLETED (3) Create a final private ListItemClickListener called mOnClickListener
-    /*
-     * An on-click handler that we've defined to make it easy for an Activity to interface with
-     * our RecyclerView
-     */
     final private ListItemClickListener mOnClickListener;
-    private static int viewHolderCount;
-    private int mNumberItems;
     private ArrayList<BluetoothDevice> mLeDevices;
 
-
-    // COMPLETED (1) Add an interface called ListItemClickListener
-    // COMPLETED (2) Within that interface, define a void method called onListItemClick that takes an int as a parameter
     /**
      * The interface that receives onClick messages.
      */
@@ -53,23 +25,18 @@ public class BLEDeviceAdapter extends RecyclerView.Adapter<BLEDeviceAdapter.Devi
         void onListItemClick(int clickedItemIndex);
     }
 
-    // COMPLETED (4) Add a ListItemClickListener as a parameter to the constructor and store it in mOnClickListener
     /**
      * Constructor for BLEDeviceAdapter that accepts a number of items to display and the specification
      * for the ListItemClickListener.
      *
-     * @param numberOfItems Number of items to display in list
      * @param listener Listener for list item clicks
      */
-    public BLEDeviceAdapter(int numberOfItems, ListItemClickListener listener) {
-        mNumberItems = numberOfItems;
+    public BLEDeviceAdapter(ListItemClickListener listener) {
         mOnClickListener = listener;
-        viewHolderCount = 0;
         mLeDevices = new ArrayList<BluetoothDevice>();
     }
 
     /**
-     *
      * This gets called when each new ViewHolder is created. This happens when the RecyclerView
      * is laid out. Enough ViewHolders will be created to fill the screen and allow for scrolling.
      *
@@ -82,20 +49,13 @@ public class BLEDeviceAdapter extends RecyclerView.Adapter<BLEDeviceAdapter.Devi
      */
     @Override
     public DeviceViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        Log.i(TAG, "onCreateViewHolder");
         Context context = viewGroup.getContext();
         int layoutIdForListItem = R.layout.listitem_device;
         LayoutInflater inflater = LayoutInflater.from(context);
-        boolean shouldAttachToParentImmediately = false;
 
-        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, false);
         DeviceViewHolder viewHolder = new DeviceViewHolder(view);
 
-//        viewHolder.deviceAddress.setText("ViewHolder index: " + viewHolderCount);
-
-        viewHolderCount++;
-        Log.i(TAG, "onCreateViewHolder: number of ViewHolders created: "
-                + viewHolderCount);
         return viewHolder;
     }
 
@@ -111,10 +71,8 @@ public class BLEDeviceAdapter extends RecyclerView.Adapter<BLEDeviceAdapter.Devi
      */
     @Override
     public void onBindViewHolder(DeviceViewHolder holder, int position) {
-        Log.d(TAG, "#" + position);
-        holder.bind(position);
-//        holder.deviceName.setText(mLeDevices.get(position).getName());
-//        holder.deviceAddress.setText(mLeDevices.get(position).getAddress());
+        holder.deviceName.setText(mLeDevices.get(position).getName());
+        holder.deviceAddress.setText(mLeDevices.get(position).getAddress());
     }
 
     /**
@@ -125,13 +83,14 @@ public class BLEDeviceAdapter extends RecyclerView.Adapter<BLEDeviceAdapter.Devi
      */
     @Override
     public int getItemCount() {
-        return mNumberItems;
+        return mLeDevices.size();
     }
 
     public void addDevice(BluetoothDevice device) {
-        if(!mLeDevices.contains(device) && device.getName() != null) {
+        if (!mLeDevices.contains(device) && device.getName() != null) {
             mLeDevices.add(device);
         }
+        this.notifyDataSetChanged();
     }
 
     public BluetoothDevice getDevice(int position) {
@@ -142,22 +101,20 @@ public class BLEDeviceAdapter extends RecyclerView.Adapter<BLEDeviceAdapter.Devi
         mLeDevices.clear();
     }
 
-    // COMPLETED (5) Implement OnClickListener in the DeviceViewHolder class
     /**
      * Cache of the children views for a list item.
      */
     class DeviceViewHolder extends RecyclerView.ViewHolder
             implements OnClickListener {
 
-        // Will display the position in the list, ie 0 through getItemCount() - 1
         TextView deviceName;
-        // Will display which ViewHolder is displaying this data
         TextView deviceAddress;
 
         /**
          * Constructor for our ViewHolder. Within this constructor, we get a reference to our
          * TextViews and set an onClickListener to listen for clicks. Those will be handled in the
          * onClick method below.
+         *
          * @param itemView The View that you inflated in
          *                 {@link BLEDeviceAdapter#onCreateViewHolder(ViewGroup, int)}
          */
@@ -166,23 +123,12 @@ public class BLEDeviceAdapter extends RecyclerView.Adapter<BLEDeviceAdapter.Devi
 
             deviceName = itemView.findViewById(R.id.device_name);
             deviceAddress = itemView.findViewById(R.id.device_address);
-            // COMPLETED (7) Call setOnClickListener on the View passed into the constructor (use 'this' as the OnClickListener)
             itemView.setOnClickListener(this);
         }
 
         /**
-         * A method we wrote for convenience. This method will take an integer as input and
-         * use that integer to display the appropriate text within a list item.
-         * @param listIndex Position of the item in the list
-         */
-        void bind(int listIndex) {
-            deviceName.setText(String.valueOf(listIndex));
-        }
-
-
-        // COMPLETED (6) Override onClick, passing the clicked item's position (getAdapterPosition()) to mOnClickListener via its onListItemClick method
-        /**
          * Called whenever a user clicks on an item in the list.
+         *
          * @param v The View that was clicked
          */
         @Override
--- app/src/main/java/io/github/jacquelynoelle/padfoot/BLEScanActivity.java
+++ app/src/main/java/io/github/jacquelynoelle/padfoot/BLEScanActivity.java
@@ -20,30 +20,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
-
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// COMPLETED (8) Implement BLEDeviceAdapter.ListItemClickListener from the MainActivity
 public class BLEScanActivity extends AppCompatActivity
         implements BLEDeviceAdapter.ListItemClickListener {
 
     private final static String TAG = BLEScanActivity.class.getSimpleName();
-    private static final int NUM_LIST_ITEMS = 20;
     private BLEDeviceAdapter mAdapter;
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothLeScanner mBluetoothScanner;
@@ -52,29 +32,23 @@ public class BLEScanActivity extends AppCompatActivity
     private RecyclerView mDeviceList;
 
     private static final int REQUEST_ENABLE_BT = 1;
-    // Stops scanning after 10 seconds.
-    private static final long SCAN_PERIOD = 10000;
+    private static final long SCAN_PERIOD = 10000; // Stops scanning after 10 seconds.
     private Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blescan);
-        Log.i(TAG, "onCreate");
-        mHandler = new Handler();
 
-        /*
-         * Using findViewById, we get a reference to our RecyclerView from xml. This allows us to
-         * do things like set the adapter of the RecyclerView and toggle the visibility.
-         */
+        mHandler = new Handler();
         mDeviceList = (RecyclerView) findViewById(R.id.rv_devices);
+
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
             finish();
         }
 
-        // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
-        // BluetoothAdapter through BluetoothManager.
+        // Initializes a Bluetooth adapter and scanner
         final BluetoothManager bluetoothManager =
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         mBluetoothAdapter = bluetoothManager.getAdapter();
@@ -87,31 +61,11 @@ public class BLEScanActivity extends AppCompatActivity
             return;
         }
 
-        /*
-         * A LinearLayoutManager is responsible for measuring and positioning item views within a
-         * RecyclerView into a linear list. This means that it can produce either a horizontal or
-         * vertical list depending on which parameter you pass in to the LinearLayoutManager
-         * constructor. By default, if you don't specify an orientation, you get a vertical list.
-         * In our case, we want a vertical list, so we don't need to pass in an orientation flag to
-         * the LinearLayoutManager constructor.
-         *
-         * There are other LayoutManagers available to display your data in uniform grids,
-         * staggered grids, and more! See the developer documentation for more details.
-         */
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         mDeviceList.setLayoutManager(layoutManager);
-
-        /*
-         * Use this setting to improve performance if you know that changes in content do not
-         * change the child layout size in the RecyclerView
-         */
         mDeviceList.setHasFixedSize(true);
 
-        // COMPLETED (13) Pass in this as the ListItemClickListener to the BLEDeviceAdapter constructor
-        /*
-         * The BLEDeviceAdapter is responsible for displaying each item in the list.
-         */
-        mAdapter = new BLEDeviceAdapter(NUM_LIST_ITEMS, this);
+        mAdapter = new BLEDeviceAdapter( this);
         mDeviceList.setAdapter(mAdapter);
 
         scanLeDevice(true);
@@ -143,12 +97,6 @@ public class BLEScanActivity extends AppCompatActivity
             case R.id.menu_stop:
                 scanLeDevice(false);
                 break;
-                /*
-             * When you click the reset menu item, we want to start all over
-             * and display the pretty gradient again. There are a few similar
-             * ways of doing this, with this one being the simplest of those
-             * ways. (in our humble opinion)
-             */
 //            case R.id.action_refresh:
 //                // COMPLETED (14) Pass in this as the ListItemClickListener to the BLEDeviceAdapter constructor
 //                mAdapter = new BLEDeviceAdapter(NUM_LIST_ITEMS, this);
@@ -173,7 +121,7 @@ public class BLEScanActivity extends AppCompatActivity
         }
 
         // Initializes list view adapter.
-        mAdapter = new BLEDeviceAdapter(NUM_LIST_ITEMS, this);
+        mAdapter = new BLEDeviceAdapter( this);
         mDeviceList.setAdapter(mAdapter);
         scanLeDevice(true);
     }
@@ -195,7 +143,6 @@ public class BLEScanActivity extends AppCompatActivity
         mAdapter.clear();
     }
 
-    // COMPLETED (10) Override ListItemClickListener's onListItemClick method
     /**
      * This is where we receive our callback from
      * {@link io.github.jacquelynoelle.padfoot.BLEDeviceAdapter.ListItemClickListener}
@@ -206,36 +153,13 @@ public class BLEScanActivity extends AppCompatActivity
      */
     @Override
     public void onListItemClick(int clickedItemIndex) {
-        // COMPLETED (11) In the beginning of the method, cancel the Toast if it isn't null
-        /*
-         * Even if a Toast isn't showing, it's okay to cancel it. Doing so
-         * ensures that our new Toast will show immediately, rather than
-         * being delayed while other pending Toasts are shown.
-         *
-         * Comment out these three lines, run the app, and click on a bunch of
-         * different items if you're not sure what I'm talking about.
-         */
-        if (mToast != null) {
-            mToast.cancel();
-        }
-
-        // COMPLETED (12) Show a Toast when an item is clicked, displaying that item number that was clicked
-        /*
-         * Create a Toast and store it in our Toast field.
-         * The Toast that shows up will have a message similar to the following:
-         *
-         *                     Item #42 clicked.
-         */
-        String toastMessage = "Item #" + clickedItemIndex + " clicked.";
-        mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
-
-        mToast.show();
-
         final BluetoothDevice device = mAdapter.getDevice(clickedItemIndex);
         if (device == null) return;
+
         final Intent intent = new Intent(this, DeviceControlActivity.class);
         intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, device.getName());
         intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
+
         if (mScanning) {
             mBluetoothScanner.stopScan(mLeScanCallback);
             mScanning = false;
@@ -264,28 +188,10 @@ public class BLEScanActivity extends AppCompatActivity
         invalidateOptionsMenu();
     }
 
-    // Device scan callback.
-//    private BluetoothAdapter.LeScanCallback OLDmLeScanCallback =
-//            new BluetoothAdapter.LeScanCallback() {
-//
-//                @Override
-//                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
-//                    runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            mAdapter.addDevice(device);
-//                            mAdapter.notifyDataSetChanged();
-//                        }
-//                    });
-//                }
-//            };
-
     private ScanCallback mLeScanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
-            Log.i(TAG, "onScanResult");
             mAdapter.addDevice(result.getDevice());
-//            mAdapter.notifyDataSetChanged(); // having this in there created a zillion viewholders
             super.onScanResult(callbackType, result);
         }
     };
