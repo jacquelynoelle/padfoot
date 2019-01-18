32dff97e4cc3352d63fecefe212cd28c75ff4567
jackie <jacquelyn.cheng@me.com>
2019-01-18 15:10:05 -0800

    added daily chart - not live data yet

--- app/src/main/java/io/github/jacquelynoelle/padfoot/activities/MainActivity.java
+++ app/src/main/java/io/github/jacquelynoelle/padfoot/activities/MainActivity.java
@@ -110,7 +110,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
         attachDatabaseReadListener();
-        loadTodaysChart();
+        loadHourlyChart();
+        loadWeeklyChart();
         super.onResume();
     }
 
@@ -166,8 +167,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void loadTodaysChart() {
-        BarChart chart = (BarChart) findViewById(R.id.chart1);
+    private void loadHourlyChart() {
+        BarChart chart = (BarChart) findViewById(R.id.chart_hourly);
         chart.setTouchEnabled(true);
         chart.setDragEnabled(false);
         chart.setScaleEnabled(false);
@@ -178,22 +179,23 @@ public class MainActivity extends AppCompatActivity {
         chart.setDragDecelerationEnabled(false);
 
         ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(1, 3));
-        entries.add(new BarEntry(2, 1));
-        entries.add(new BarEntry(3, 2));
-        entries.add(new BarEntry(4, 3));
-        entries.add(new BarEntry(5, 4));
-        entries.add(new BarEntry(6, 5));
-        entries.add(new BarEntry(7, 3));
-        entries.add(new BarEntry(8, 1));
-        entries.add(new BarEntry(9, 2));
-        entries.add(new BarEntry(10, 3));
-        entries.add(new BarEntry(11, 4));
-        entries.add(new BarEntry(12, 5));
-        entries.add(new BarEntry(13, 3));
-        entries.add(new BarEntry(14, 1));
-        entries.add(new BarEntry(15, 2));
-        entries.add(new BarEntry(16, 3));
+        entries.add(new BarEntry(0, 3));
+        entries.add(new BarEntry(1, 1));
+        entries.add(new BarEntry(2, 2));
+        entries.add(new BarEntry(3, 3));
+        entries.add(new BarEntry(4, 4));
+        entries.add(new BarEntry(5, 5));
+        entries.add(new BarEntry(6, 3));
+        entries.add(new BarEntry(7, 1));
+        entries.add(new BarEntry(8, 2));
+        entries.add(new BarEntry(9, 3));
+        entries.add(new BarEntry(10, 4));
+        entries.add(new BarEntry(11, 5));
+        entries.add(new BarEntry(12, 3));
+        entries.add(new BarEntry(13, 1));
+        entries.add(new BarEntry(14, 2));
+        entries.add(new BarEntry(15, 3));
+        entries.add(new BarEntry(16, 0));
         entries.add(new BarEntry(17, 0));
         entries.add(new BarEntry(18, 0));
         entries.add(new BarEntry(19, 0));
@@ -201,7 +203,6 @@ public class MainActivity extends AppCompatActivity {
         entries.add(new BarEntry(21, 0));
         entries.add(new BarEntry(22, 0));
         entries.add(new BarEntry(23, 0));
-        entries.add(new BarEntry(24, 0));
 
         BarDataSet barDataSet = new BarDataSet(entries, "Cells");
         barDataSet.setHighlightEnabled(true);
@@ -212,7 +213,78 @@ public class MainActivity extends AppCompatActivity {
         XAxis xAxis = chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setDrawGridLines(false);
-        xAxis.setAxisMinimum(0);
+        xAxis.setAxisMinimum(-1);
+        xAxis.setLabelCount(6);
+
+        chart.getAxisLeft().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+        chart.getAxisLeft().setAxisMinimum(0f);
+
+        chart.getLegend().setEnabled(false);   // Hide the legend
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("Dec");
+        labels.add("Nov");
+        labels.add("Oct");
+        labels.add("Sep");
+        labels.add("Aug");
+        labels.add("Jul");
+        labels.add("Jun");
+        labels.add("May");
+        labels.add("Apr");
+        labels.add("Mar");
+        labels.add("Feb");
+        labels.add("Jan");
+
+        BarData data = new BarData(barDataSet);
+        chart.setData(data); // set the data and list of lables into chart
+
+        Description description = new Description();
+        description.setText("");
+        chart.setDescription(description); // set the description
+        chart.setDrawBorders(false);
+
+        int[] colors = new int[]{
+                R.color.colorAccent,
+                R.color.colorAccentLight,
+        };
+        barDataSet.setColors(ColorTemplate.createColors(getResources(), colors));
+
+        chart.invalidate();
+        chart.animateY(3000);
+    }
+
+
+    private void loadWeeklyChart() {
+        BarChart chart = (BarChart) findViewById(R.id.chart_weekly);
+        chart.setTouchEnabled(true);
+        chart.setDragEnabled(false);
+        chart.setScaleEnabled(false);
+        chart.setScaleXEnabled(false);
+        chart.setScaleYEnabled(false);
+        chart.setPinchZoom(false);
+        chart.setDoubleTapToZoomEnabled(false);
+        chart.setDragDecelerationEnabled(false);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, 3));
+        entries.add(new BarEntry(1, 1));
+        entries.add(new BarEntry(2, 2));
+        entries.add(new BarEntry(3, 3));
+        entries.add(new BarEntry(4, 4));
+        entries.add(new BarEntry(5, 5));
+        entries.add(new BarEntry(6, 3));
+
+        BarDataSet barDataSet = new BarDataSet(entries, "Cells");
+        barDataSet.setHighlightEnabled(true);
+        barDataSet.setDrawValues(false);
+        barDataSet.setHighLightColor(ContextCompat.getColor(this, R.color.colorPrimaryLight));
+        barDataSet.setHighLightAlpha(255);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setAxisMinimum(-0.5f);
         xAxis.setLabelCount(6);
 
         chart.getAxisLeft().setEnabled(false);
